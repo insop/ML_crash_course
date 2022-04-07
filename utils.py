d4ed7d2ca7d58498e72ed6b5c35b26f58c9d280d
Insop Song <insop.song@gmail.com>
2022-04-07 00:07:50 -0700

    Added nn code

--- utils.py
+++ utils.py
@@ -7,8 +7,8 @@ import pandas as pd # structured data handling
 import sklearn.linear_model
 import os
 
-def save_fig(fig_id, tight_layout=True, imabes_path='figures', fig_extension="png", resolution=300):
-    path = os.path.join(imabes_path, fig_id + "." + fig_extension)
+def save_fig(fig_id, tight_layout=True, images_path='figures', fig_extension="png", resolution=300):
+    path = os.path.join(images_path, fig_id + "." + fig_extension)
     print("Saving figure", fig_id)
     if tight_layout:
         plt.tight_layout()
